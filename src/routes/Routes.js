31b0ac26e441e65608f1341c055d241d90d4d980
HellersonMurilo <calabianquimurilo328@gmail.com>
2024-08-17 13:53:40 -0300

    fix: created routes of Auth

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,10 @@
 const {Router} = require('express')
 const userRoutes = require('./user.Routes')
+const authRoutes = require('./auth.Routes')
 
 const router = Router()
 
 router.use('/user', userRoutes)
+router.use('/auth', authRoutes)
 
 module.exports = router
---
+++ src/routes/auth.Routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const authRoutes = Router();
+
+authRoutes.post("/signin", (req, res) => {
+    
+});
+
+authRoutes.post('/signup', (req, res)=>{
+
+})
+module.exports = authRoutes;
