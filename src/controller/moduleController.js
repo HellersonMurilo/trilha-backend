960c649e24efb98ae7560b87cba1336b10fa5150
HellersonMurilo <calabianquimurilo328@gmail.com>
2024-11-30 11:21:08 -0300

    feat: added delete and create controller modules

--- src/controller/moduleController.js
+++ src/controller/moduleController.js
@@ -1,11 +1,20 @@
-const userLearningPath = require("../models/intermediateUserLearning")
+const LearningPath = require("../models/learningPath")
 const Module = require("../models/module")
 
 const moduleController = {
-    createModule: async (req, res) =>{
+    createModule: async (req, res) => {
+
+        const { nameModule, descriptionModule, quantLessons } = req.body
+        const trailId = req.params.idTrail
+        
         try {
-            const { nameModule, descriptionModule, quantLessons } = req.body
-            const trailId = req.params.idTrail
+
+            // Verificar se a trilha existe
+            const trail = await LearningPath.findOne({ where: { trailId: trailId } });
+
+            if (!trail) {
+                return res.status(404).json({ msg: 'Trilha não encontrada.' });
+            }
 
             //salvando na tabela de módulos
             const module = await Module.create({
@@ -14,6 +23,12 @@ const moduleController = {
                 descriptionModule: descriptionModule,
                 quantLessons: quantLessons
             })
+
+            //Update na tabela Trilha
+            const updateTrailModule = await LearningPath.update(
+                { quantModules: trail.quantModules + 1 },
+                { where: { trailId: trailId } }
+            )
 
             return res.status(201).json({
                 msg: 'Modulo criado com sucesso',
@@ -27,9 +42,9 @@ const moduleController = {
             })
         }
     },
-    getTrails: async (req, res) =>{
+    getTrails: async (req, res) => {
         const trailId = req.params.idTrail
-        try {            
+        try {
             //buscando a trilha na tabela de modulos
             const listModules = await Module.findAll({
                 where: {
@@ -40,12 +55,49 @@ const moduleController = {
             return res.status(200).json({
                 modules: listModules
             })
-            
+
         } catch (error) {
             return res.status(500).json({
                 msg: 'Ocorreu um erro critico ao listar as Trilhas',
                 err: error
             })
+        }
+    },
+    deleteModule: async (req, res) => {
+        const { idTrail, idModule } = req.params;
+
+        if (!idTrail || !idModule) {
+            return res.status(400).json({ msg: 'Parâmetros inválidos.' });
+        }
+
+        try {
+            // Validar se a trilha e o módulo existem
+            const trail = await LearningPath.findOne({ where: { trailId: idTrail } });
+            if (!trail) {
+                return res.status(404).json({ msg: 'Trilha não encontrada.' });
+            }
+
+            const moduleToDelete = await Module.findOne({ where: { module_id: idModule } });
+            if (!moduleToDelete) {
+                return res.status(404).json({ msg: 'Módulo não encontrado.' });
+            }
+
+            // Excluir o módulo
+            await Module.destroy({ where: { module_id: idModule } });
+
+            // Atualizar a quantidade de módulos
+            const updatedTrail = await LearningPath.update(
+                { quantModules: trail.quantModules - 1 },
+                { where: { trailId: idTrail } }
+            );
+
+            return res.status(200).json({ msg: 'Módulo deletado com sucesso.' });
+        } catch (error) {
+            console.error('Erro ao deletar o módulo:', error);
+            return res.status(500).json({
+                msg: 'Ocorreu um erro crítico ao deletar o módulo.',
+                err: error.message,
+            });
         }
     }
 }
--- src/routes/modules.Routes.js
+++ src/routes/modules.Routes.js
@@ -6,14 +6,21 @@ const moduleController = require("../controller/moduleController");
 
 const modules = Router()
 
+//LISTAR
 modules.post('/modules/:idTrail', decriptedJwt, validateIdTrilha,(req, res) => {
 
     console.log(req.params.idTrail)
     moduleController.getTrails(req, res)
 })
 
-modules.post('/:idTrail/create', validateFields, decriptedJwt, validateIdTrilha, (req, res) => {
+//CRIAR
+modules.post('/create/:idTrail', validateFields, decriptedJwt, validateIdTrilha, (req, res) => {
     moduleController.createModule(req, res)
+})
+
+//DELETAR
+modules.delete('/delete/:idTrail/:idModule', decriptedJwt, validateIdTrilha, (req, res) => {
+    moduleController.deleteModule(req, res)
 })
 
 module.exports = modules
